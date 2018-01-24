8ef376729d991c4a74317691899d7b18a7d119e3
Munis Isazade <munisisazade@gmail.com>
2018-01-24 17:31:45 +0400

    Add configurarion

---
+++ settings.py
@@ -0,0 +1,16 @@
+# Following variable add your django application settings.py
+
+# Milli kart Transaction registration
+
+MID = '<Your mid name>' # base Merchant ID
+PAYMENT_SECRET_KEY = '<Milli card secret key>' # value is given by Millikart
+
+TEST_MILLI_KART_PAYMENT_HTTP = 'http://***********'
+TEST_MILLI_KART_PAYMENT_HTTPS = 'https://***********'
+MILLI_KART_PAYMENT = 'https://**********'
+
+TEST_MILLI_KART_PAYMENT_STATUS_HTTP = 'http://***********'
+TEST_MILLI_KART_PAYMENT_STATUS_HTTPS = 'https://***********'
+MILLI_KART_PAYMENT_STATUS = 'https://**********'
+
+
